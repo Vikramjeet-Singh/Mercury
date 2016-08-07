36dbd3d5754fcd0e9c6ef043815b833d52900908
Vikramjeet Singh <viksingh@apple.com>
2016-08-07 01:01:59 -0700

    added more files

---
+++ Mercury/ImageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ImageViewController.swift
+//  Mercury
+//
+//  Created by Vikramjeet Singh on 8/7/16.
+//  Copyright © 2016 Vikramjeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
---
+++ Mercury/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Mercury
+//
+//  Created by Vikramjeet Singh on 8/7/16.
+//  Copyright © 2016 Vikramjeet Singh. All rights reserved.
+//
+
+import Foundation
